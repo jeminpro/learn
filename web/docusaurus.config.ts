f5b18fbe088c97e1c4a14d1d3f7af6667825bff0
Jemin Pathazhapurakkal <jemsworld@gmail.com>
2025-10-08 17:08:17 +0000

    Add support for math rendering with KaTeX and remark-math

--- web/docusaurus.config.ts
+++ web/docusaurus.config.ts
@@ -1,6 +1,8 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
@@ -52,6 +54,8 @@ const config: Config = {
           // Remove this to remove the "edit this page" links.
           editUrl:
             'https://github.com/jeminpro/learn/tree/main/web/',
+          remarkPlugins: [remarkMath],
+          rehypePlugins: [rehypeKatex],
         },
         theme: {
           customCss: './src/css/custom.css',
